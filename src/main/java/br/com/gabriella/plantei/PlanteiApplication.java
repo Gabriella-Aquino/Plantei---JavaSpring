5745a9c50004b055d5f69b2cd5d55759d6fd22a5
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-11-19 20:38:10 -0300

    first commit

---
+++ src/main/java/br/com/gabriella/plantei/PlanteiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriella.plantei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanteiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanteiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabriella/plantei/PlanteiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriella.plantei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanteiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
