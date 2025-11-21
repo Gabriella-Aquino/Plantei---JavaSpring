e9e439c43e92dad2faf3b84f5aace5b093f09c0b
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-11-21 17:40:42 -0300

    feat: add dtos para GardenMember

---
+++ src/main/java/br/com/gabriella/dtos/GardenMember/GardenMemberReadDTO.java
@@ -0,0 +1,16 @@
+package br.com.gabriella.dtos.GardenMember;
+
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GardenMemberReadDTO {
+  private Long id;
+  private Long gardenId;
+  private Long userId;
+  private LocalDate entryDate;
+}
