66a1bb476df8951c2e1cdec3b6b240cf3a577ff3
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-11-21 17:41:10 -0300

    feat: add dtos para EventPlant

---
+++ src/main/java/br/com/gabriella/dtos/EventPlant/EventPlantCreateDTO.java
@@ -0,0 +1,20 @@
+package br.com.gabriella.dtos.EventPlant;
+
+import java.time.LocalDateTime;
+
+import br.com.gabriella.plantei.model.enums.EventType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventPlantCreateDTO {
+  private Long plantUserId;
+  private Long userId;
+  private EventType type;
+  private String description;
+  private LocalDateTime eventDate;
+}
