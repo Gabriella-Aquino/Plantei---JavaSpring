329e3987e69984758b46209bd6a0c14e41b9d072
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-11-26 11:47:58 -0300

    feat: adiciona PlantService

--- src/main/java/br/com/gabriella/plantei/service/PlantService.java
+++ src/main/java/br/com/gabriella/plantei/service/PlantService.java
@@ -1,5 +1,7 @@
 package br.com.gabriella.plantei.service;
 
+import br.com.gabriella.plantei.dtos.Plant.PlantUpdateDTO;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,8 @@ import br.com.gabriella.plantei.dtos.Plant.PlantReadDTO;
 import br.com.gabriella.plantei.mapper.PlantMapper;
 import br.com.gabriella.plantei.model.Plant;
 import br.com.gabriella.plantei.repository.PlantRepository;
+
+import java.util.List;
 
 @Service
 public class PlantService {
@@ -18,9 +22,25 @@ public class PlantService {
   @Autowired
   private PlantMapper plantMapper;
 
-  public PlantReadDTO create(PlantCreateDTO dto) {
-    Plant plant = plantMapper.toEntity(dto);
+  public PlantReadDTO createPlant(PlantCreateDTO data) {
+    Plant plant = plantMapper.toEntity(data);
     plantRepository.save(plant);
     return plantMapper.toReadDTO(plant);
+  }
+
+  public  PlantReadDTO getPlantById(long id){
+      Plant plant = plantRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Planta não encontrada"));
+      return plantMapper.toReadDTO(plant);
+  }
+
+  public List<PlantReadDTO> listAllPlants(){
+      return plantRepository.findAll().stream().map(plantMapper::toReadDTO).toList();
+  }
+
+  public PlantReadDTO updatePlant(long id, PlantUpdateDTO data){
+      Plant plant = plantRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Planta não encontrada"));
+      plantMapper.updateEntityFromDTO(data, plant);
+      Plant update =plantRepository.save(plant);
+      return plantMapper.toReadDTO(update);
   }
 }
